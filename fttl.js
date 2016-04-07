39a92874c2c8d182806bfa4969c0278e1daaaad8
Liam O'Connor <liamoconnor1247@gmail.com>
2016-04-08 09:33:21 +1200

    Got the images changing but need to improve performance by using z index rather than loading images each time

--- fttl.js
+++ fttl.js
@@ -3,28 +3,29 @@ var image_change;
 var current_image = 0;
 var img_array = new Array();
 
-img_array[0] = 'resources/orange_sunset.JPG';
-img_array[1] = 'resources/Queenstown_winter_aerial';
-img_array[2] = 'resources/Queenstown-aerial-in-winter-dusk.JPG';
+img_array[0] = new Image(); 
+img_array[1] = new Image(); 
+img_array[2] = new Image(); 
+
+img_array[0] = "url('resources/orange_sunset.JPG')";
+img_array[1] = "url('resources/Queenstown_winter_aerial.JPG')";
+img_array[2] = "url('resources/Queenstown-aerial-in-winter-dusk.JPG')";
 
 function choose_image() {
-    var image = document.getElementsByTagName('html')[0].style.backgroundImage;
-    console.log(document.getElementsByTagName('html')[0].style.backgroundImage);
-
-
     if (current_image == 0) {
     	current_image = 1;
-        image.src = img_array[1];
+        document.body.style.backgroundImage = img_array[current_image];
     } else if (current_image == 1){
     	current_image = 2;
-        image.src = img_array[2];
+        document.body.style.backgroundImage = img_array[current_image];
     }
     else if (current_image == 2){
     	current_image = 0;
-    	image.src = img_array[0];
+    	document.body.style.backgroundImage = img_array[current_image];
     }
     change_image();
 }
 
 function change_image(){
 	image_change = setTimeout(choose_image, 2500);
+}
